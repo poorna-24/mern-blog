7ae9a6e79e557ad16213c50979fda6a7805a073e
Poorna chander Terala <60097572+poorna-24@users.noreply.github.com>
2024-03-08 11:42:59 +0530

    user model

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
 dotenv.config();
 
 app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((error) => console.log(error.message));
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on port ${process.env.PORT}`);
