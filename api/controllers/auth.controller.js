29f1e88005ea1e00f5d738ec484a33077b040e00
Poorna chander Terala <60097572+poorna-24@users.noreply.github.com>
2024-03-13 12:56:03 +0530

    update
    update

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,7 +7,6 @@ export const signup = async (req, res, next) => {
   if (!username || !email || !password || username === "" || email === "" || password === "") {
     next(errorHandler(400, "All fields are required"));
   }
-
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
