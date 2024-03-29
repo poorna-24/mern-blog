8fd74650609e40dca42d1fb8b8b1edc78e4c0405
Poorna chander Terala <60097572+poorna-24@users.noreply.github.com>
2024-03-29 12:41:58 +0530

    image to url

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Button, TextInput } from "flowbite-react";
 import { useSelector } from "react-redux";
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setimageFile] = useState(null);
+  const [imageFileUrl, setimageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setimageFile(file);
+      setimageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  // console.log(imageFile, imageFileUrl);
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-          <img src={currentUser.profilePicture} alt="user" className="rounded-full w-full h-full border-8 border-[lightgray]" />
+        <input type="file" accept="image/*" onChange={handleImageChange} ref={filePickerRef} hidden />
+
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
+          <img src={imageFileUrl || currentUser.profilePicture} alt="user" className="rounded-full w-full h-full border-8 border-[lightgray]" />
         </div>
         <TextInput type="text" id="username" placeholder="username" defaultValue={currentUser.username} />
         <TextInput type="email" id="email" placeholder="email" defaultValue={currentUser.email} />
